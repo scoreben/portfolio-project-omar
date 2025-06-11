ae76c21e6bb44965916f36c5e823f55e37745632
scoreben <omarshebl2016@gmail.com>
2025-06-11 07:41:05 -0500

    update url

--- src/app/page.js
+++ src/app/page.js
@@ -6,7 +6,7 @@ import ClientProjectView from "@/components/client-view/project";
 import Image from "next/image";
 
 async function extractAllDatas(currentSection) {
-  const res = await fetch(`http://localhost:3000/api/${currentSection}/get`,{
+  const res = await fetch(`https://omarportfolio.netlify.app/api/${currentSection}/get`,{
     method: "GET",
     cache: "no-store"
   });
